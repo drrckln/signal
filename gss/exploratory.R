1ba9bcf6dbd58c5ea5d68caa9e3137ebfdb98c45
Derrick Lin <drrckln@gmail.com>
2016-06-15 11:57:13 -0700

    oh whew coherency comes back if we use svd imputation

--- gss/exploratory.R
+++ gss/exploratory.R
@@ -15,9 +15,6 @@ library("softImpute")
 scree = function(df) {
   cor_matrix = cor(df, use="pairwise.complete.obs")
   cor_matrix[is.na(cor_matrix)] = 0
-  
-  eig_cor_matrix = eigen(cor_matrix)
-  qplot(1:80, eig_cor_matrix$values[order(abs(eig_cor_matrix$values), decreasing=TRUE)][1:80])
   return(cor_matrix)
 }
 
@@ -29,7 +26,6 @@ factor_analysis = function(c_matrix, num_components) {
   return(fa_df)
 }
 
-
 # Nat -> Loadings -> Keys -> Matrix of [Questions], [Codings]
 getQuestions = function(n_qs=10, loadings, keys) {
   fact_questions = lapply(loadings, function(c) keys$values[order(abs(c), decreasing=TRUE)][1:n_qs])
@@ -39,24 +35,30 @@ getQuestions = function(n_qs=10, loadings, keys) {
 
 
 ### MAIN ### 
-df = read.csv("~/repos/signal/gss/gss.csv")
-#sels = select(df, -X, -year, -oversamp, -formwt, -relate1, -relhhd1, -relhh1)
+df = read.csv("~/repos/signal/gss/gss_old.csv")
+df = df %>% select(-X, -id, -formwt, -relhh1, -relhhd1, -relhh2, -relhhd2, -oversamp, -sampcode, -year)
 df = df %>% filter(sex == 2)
 keys = read.csv("~/repos/signal/gss/keys.csv")
 keys = keys[keys$X %in% names(df),]
 keys = keys[order(keys$X),]
 
+# Conduct a scree test: 6, 7, 9, or 12
 cm = scree(df)
 eig_cor_matrix = eigen(cm)
 qplot(1:80, eig_cor_matrix$values[order(abs(eig_cor_matrix$values), decreasing=TRUE)][1:80])
-
-# Conduct a scree test: 6, 7, 9, or 12
-scree = 12
+scree = 14
 
 factors = factor_analysis(cm, scree)
 qplot(1:12, eigen(factors$score.cor))
 
-# the rotation matrix needed after imputation
+# the rotation matrix; needed after imputation
 loadings = as.data.frame(factors$loadings[,1:scree])
 
 qs = getQuestions(10, loadings, keys)
+qs[,1]
+
+# imputation
+fit = softImpute(as.matrix(df), rank.max=15, lambda = 9, type="svd")
+ndf = as.data.frame(complete(as.matrix(df), fit))
+df = ndf
+
